60d0dc521fef54278112aaa50b407a7b216f79bc
againle <u3612776@connect.hku.hk>
2025-06-06 11:57:08 +0800

    Add dashboard and customer pages with layout and navigation components

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-6">
+      <h1 className="text-3xl font-bold">Customer Page</h1>
+      <p className="mt-4 text-lg">This is the customer page.</p>
+      <p className="mt-2 text-sm text-gray-500">
+        You can add your customers components here.
+      </p>
+    </main>
+  );
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-6">
+      <h1 className="text-3xl font-bold">Invoice Page</h1>
+      <p className="mt-4 text-lg">This is the Invoice page.</p>
+      <p className="mt-2 text-sm text-gray-500">
+        You can add your Invoice components here.
+      </p>
+    </main>
+  );
+}
